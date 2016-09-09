efdd5cf431985966e7673a11988e106517a3f0ed
Luis Villasenor <sectureags@gmail.com>
2016-09-09 08:00:05 -0500

    Agrupa por Taller y fecha y agrega sede, ubicacion, hora ini y hora fin

--- applications/cedula/views/include/subactividad.php
+++ applications/cedula/views/include/subactividad.php
@@ -1,3 +1,17 @@
+
+<?php 
+#$anterior = "";
+#$actual = $necs->subactividad;
+#$anterior = ($actual != $anterior ) ? echo $actual : break ;
+#$actual = $necs->hora_ini;
+#$anterior = ($actual != $anterior ) ? echo $actual : break ;
+
+
+?>
+
+
+
+
 <b><?php echo $necs->subactividad;?></b> [
 <small><?php echo $necs->status_subact;?></small>]</br>
 <small><?php echo $necs->sede;?> </small><small> (<?php echo $necs->ubicacion;?>)</small><br>
